de45335f2dcc7ed09fa345ab3c6c27e0e7c8e648
Lucas <lucas.ferreira@blueshift.com.br>
2023-07-12 22:15:30 -0300

    Connecting with postgres

--- main.go
+++ main.go
@@ -1,11 +1,32 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"net/http"
+	_ "github.com/lib/pq"
 )
 
+type Products struct {
+	Id			int
+	Name		string
+	Description	string
+	Price 		float64
+	Quantity	int
+} 
+
 var readTemplates = template.Must(template.ParseGlob("templates/*.html")) //le todos templates que tem .html
+
+
+func connPostgres() *sql.DB {
+	connection := "user=postgres dbname=golang-marketplace password=admin host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", connection)
+	if err != nil {
+		panic(err)
+	}
+	return db
+
+}
 
 func main() {
 	http.HandleFunc("/", index)   //toda vez que chamar a barra, executo a func index
@@ -14,5 +35,33 @@ func main() {
 
 
 func index(w http.ResponseWriter, r *http.Request){  //(w writer, r responser)
-	readTemplates.ExecuteTemplate(w, "index", nil)
+	
+	db := connPostgres()
+	selectProducts, err :=db.Query("select * from tbproducts")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	eachProduct := Products{}
+	allProducts := []Products{}
+	for selectProducts.Next(){
+		var id, quantity int
+		var productname, description string
+		var price float64
+
+		err = selectProducts.Scan(&id, &productname, &description, &price, &quantity)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		eachProduct.Name = productname
+		eachProduct.Description = description
+		eachProduct.Price = price
+		eachProduct.Quantity = quantity
+
+		allProducts = append(allProducts, eachProduct)
+	}
+
+	readTemplates.ExecuteTemplate(w, "index", allProducts)
+	defer db.Close()
 }
